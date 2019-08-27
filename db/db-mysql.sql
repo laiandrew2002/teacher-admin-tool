5f73484189a354f7679a1465318aee23ed4c1fd1
unknown <laiandrew@hotmail.com>
2019-08-27 21:39:49 +0800

    third commit

--- db/db-mysql.sql
+++ db/db-mysql.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `school` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `school`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: school
@@ -14,6 +16,62 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `is_suspend` tinyint(1) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (4,'Studenteshan','studenteshan@example.com',1,'2019-08-25 00:23:07','2019-08-25 00:52:15'),(5,'Studenthon','studenthon@example.com',1,'2019-08-25 00:23:07','2019-08-26 00:46:54'),(6,'Studentjon','studentjon@example.com',0,'2019-08-26 21:22:32','2019-08-26 21:22:32'),(7,'Studentalek','studentalek@example.com',0,'2019-08-26 21:23:37','2019-08-26 21:23:37'),(8,'Studentpam','studentpam@example.com',0,'2019-08-26 21:23:37','2019-08-26 21:23:37'),(9,'Studenthaha','studenthaha@gmail.com',0,'2019-08-26 21:23:37','2019-08-26 21:23:37');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teachers`
+--
+
+LOCK TABLES `teachers` WRITE;
+/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+INSERT INTO `teachers` VALUES (1,'Teacherken','teacherken@gmail.com','2019-08-23 16:08:58','2019-08-23 16:08:58'),(2,'TeacherMolo','teacherMolo@gmail.com','2019-08-24 15:14:19','2019-08-24 15:14:19'),(3,'Teachermel','teachermel@gmail.com','2019-08-26 21:23:37','2019-08-26 21:23:37');
+/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `teachers_students`
@@ -55,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-27 21:04:41
+-- Dump completed on 2019-08-27 21:35:30
